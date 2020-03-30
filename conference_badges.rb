17963ea7f55536cd73dc2601f4440057045304c1
Vladimir J <vladdaimpaler@protonmail.com>
2020-03-30 08:09:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  returnedArray=[]
+  array.each{ |name|
+  returnedArray << badge_maker(name)
+  }
+  return returnedArray
+end
+
+def assign_rooms(array)
